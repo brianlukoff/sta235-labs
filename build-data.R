a4ce5c3ca6bb23dccf1f7d64d7bbd3d5af2be902
Brian Lukoff <brian.lukoff@gmail.com>
2024-09-23 16:07:36 -0500

    Update data file.

--- build-data.R
+++ build-data.R
@@ -1,2 +1,6 @@
+# Recreate the Rdata file with all of the data sets.
+rm(list=ls())
 profs <- read.csv("data/profs.csv")
-save.image(file="sta235.Rdata")
+colleges <- read.csv("data/colleges.csv")
+baseball <- read.table("data/RunsPerGame.txt", header=T)
+save.image("sta235.Rdata")
